5e567b4dcff6f3bd74bb53e8de64507c2f533dd9
csanchezarisa <61799628+csanchezarisa@users.noreply.github.com>
2021-04-06 23:22:47 +0200

    Navegacion custom funcional

--- app/src/main/java/com/example/buidemsl/ui/tipos/TiposViewModel.java
+++ app/src/main/java/com/example/buidemsl/ui/tipos/TiposViewModel.java
@@ -1,16 +1,16 @@
-package com.example.buidemsl.ui.gallery;
+package com.example.buidemsl.ui.tipos;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class TiposViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public TiposViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is slideshow fragment");
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/com/example/buidemsl/ui/zonas/ZonasFragment.java
+++ app/src/main/java/com/example/buidemsl/ui/zonas/ZonasFragment.java
@@ -1,4 +1,4 @@
-package com.example.buidemsl.ui.slideshow;
+package com.example.buidemsl.ui.zonas;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.buidemsl.R;
 
-public class SlideshowFragment extends Fragment {
+public class ZonasFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    private ZonasViewModel zonasViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        zonasViewModel =
+                new ViewModelProvider(this).get(ZonasViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_zonas, container, false);
+        final TextView textView = root.findViewById(R.id.text_gallery);
+        zonasViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
