d5224135374f0fdfbe2b0b220a652294cc8f1915
csanchezarisa <61799628+csanchezarisa@users.noreply.github.com>
2021-04-07 13:13:13 +0200

    CRUD zonas

--- app/src/main/java/com/example/buidemsl/models/datasource/MainDatasource.java
+++ app/src/main/java/com/example/buidemsl/models/datasource/MainDatasource.java
@@ -1,5 +1,6 @@
 package com.example.buidemsl.models.datasource;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -26,6 +27,8 @@ public class MainDatasource {
         super.finalize();
     }
 
+    /** Consulta que devuelve todas las zonas
+     * @return Cursor con las zonas que hay en la BBDD */
     public Cursor getZonas() {
         Cursor zonas = dbR.query(
                 BuidemHelper.TABLE_ZONA,
@@ -38,6 +41,77 @@ public class MainDatasource {
         );
 
         return zonas;
+    }
+
+    /** Consulta que devuelve una zona buscada por ID
+     * @param id long con el id a buscar
+     * @return Cursor con la zona buscada */
+    public Cursor getZona(long id) {
+        Cursor zona = dbR.query(
+                BuidemHelper.TABLE_ZONA,
+                new String[]{BuidemHelper.ZONA_ID, BuidemHelper.ZONA_DESCRIPCIO},
+                BuidemHelper.ZONA_ID + " = " + id,
+                null,
+                null,
+                null,
+                null
+        );
+
+        return zona;
+    }
+
+    /** Actualiza la información de una zona
+     * @param id long con el id de la zona a actualizar
+     * @param descripcion String con el contenido de la nueva
+     * descripción
+     * @return int con el número de filas afectadas*/
+    public int updateZona(long id, String descripcion) {
+
+        ContentValues values = new ContentValues();
+        values.put(BuidemHelper.ZONA_DESCRIPCIO, descripcion);
+
+        int rows = dbW.update(
+                BuidemHelper.TABLE_ZONA,
+                values,
+                BuidemHelper.ZONA_ID + " = " + id,
+                null
+        );
+
+        return rows;
+    }
+
+    /** Inserta una nueva zona.
+     * @param descripcion String con la descripción
+     * de la zona a insertar
+     * @return long con el id del nuevo insert*/
+    public long insertZona(String descripcion) {
+
+        ContentValues values = new ContentValues();
+        values.put(BuidemHelper.ZONA_DESCRIPCIO, descripcion);
+
+        long id = dbW.insert(
+                BuidemHelper.TABLE_ZONA,
+                null,
+                values
+        );
+
+        return id;
+    }
+
+    /** Elimna la zona especificada
+     * @param id long con el id de
+     * la zona a eliminar
+     * @return int con el número de
+     * filas afectadas*/
+    public int deleteZona(long id) {
+
+        int rows = dbW.delete(
+                BuidemHelper.TABLE_ZONA,
+                BuidemHelper.ZONA_ID + " = " + id,
+                null
+        );
+
+        return rows;
     }
 
 }
--- app/src/main/java/com/example/buidemsl/ui/zonas/ZonasFragment.java
+++ app/src/main/java/com/example/buidemsl/ui/zonas/ZonasFragment.java
@@ -1,15 +1,20 @@
 package com.example.buidemsl.ui.zonas;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.CursorAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.buidemsl.R;
@@ -44,17 +49,109 @@ public class ZonasFragment extends Fragment {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                mostrarAlertZona(id);
             }
         });
 
         list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                return false;
+                mostrarAlertEliminarZona(id);
+                return true;
             }
         });
 
         return root;
+    }
+
+    private void mostrarAlertZona(long id) {
+        Cursor zona = datasource.getZona(id);
+
+        String alertTitle;
+        String descriptionContent = "";
+
+        if (zona.moveToFirst()) {
+            alertTitle = getString(R.string.fragment_zonas_alert_edit_title) + " " + id;
+            descriptionContent = zona.getString(zona.getColumnIndexOrThrow(BuidemHelper.ZONA_DESCRIPCIO));
+        }
+        else {
+            alertTitle = getString(R.string.fragment_zonas_alert_add_title);
+        }
+
+        AlertDialog alert = new AlertDialog.Builder(getContext()).create();
+
+        EditText input = new EditText(getContext());
+        input.setText(descriptionContent);
+        input.setHint(R.string.fragment_zonas_alert_add_description);
+
+        alert.setTitle(alertTitle);
+        alert.setView(input);
+
+        alert.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.default_alert_accept), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                long status;
+
+                if (id < 0) {
+                    status = datasource.insertZona(input.getText().toString());
+                }
+                else {
+                    status = datasource.updateZona(id, input.getText().toString());
+                }
+
+                mostrarSnackbar(status);
+                refreshList();
+            }
+        });
+
+        alert.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.default_alert_cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Nothing
+            }
+        });
+
+        alert.setButton(DialogInterface.BUTTON_NEUTRAL, getString(R.string.default_alert_delete), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mostrarAlertEliminarZona(id);
+            }
+        });
+
+        alert.show();
+    }
+
+    private void mostrarAlertEliminarZona(long id) {
+        AlertDialog alert = new AlertDialog.Builder(getContext()).create();
+
+        alert.setTitle(getString(R.string.default_alert_delete));
+        alert.setMessage(getString(R.string.default_alert_delete_confirmation) + " " + getString(R.string.fragment_zonas_alert_edit_title) + " " + id + "?");
+
+        alert.setButton(DialogInterface.BUTTON_POSITIVE, getString(R.string.default_alert_accept), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                long status = datasource.deleteZona(id);
+                mostrarSnackbar(status);
+                refreshList();
+            }
+        });
+
+        alert.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.default_alert_cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Nothing
+            }
+        });
+
+        alert.show();
+    }
+
+    private void mostrarSnackbar(long status) {
+
+    }
+
+    private void refreshList() {
+        adapter.changeCursor(datasource.getZonas());
     }
 }
